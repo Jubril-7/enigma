4ed2c5d48b47ffa6dc629bfd77b2b09ee4124b60
Jubril-7 <jubril495@gmail.com>
2026-02-01 10:50:29 +0100

    starting over

--- src/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const config = {
-    ownerNumber: process.env.OWNER_NUMBER || '',
-    controlGroupId: process.env.CONTROL_GROUP || '',
-    prefix: process.env.PREFIX,
-};
-
-module.exports = { config };
